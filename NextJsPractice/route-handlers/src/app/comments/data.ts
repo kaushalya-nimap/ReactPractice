5db4baff8d0cc01335ee6b057819a8e1381edcf5
kaushalya-nimap <kaushalya.p@nimapinfotech.com>
2025-04-09 17:34:00 +0530

    get post method practiced

---
+++ NextJsPractice/route-handlers/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments=[
+    {
+        id:1,
+        text:"this is first comment"
+    },
+    {
+        id:2,
+        text:"Thsi is second comment"
+    },
+    {
+        id:3,
+        text:"This is third comment"
+    }
+]
